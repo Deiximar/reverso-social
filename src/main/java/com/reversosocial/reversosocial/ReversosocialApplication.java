29ca5411fa7b87aacc2747424730354cbdf9af1d
Deiximar Virriel <deiximar@gmail.com>
2024-09-16 11:54:54 +0200

    first commit

---
+++ src/main/java/com/reversosocial/reversosocial/ReversosocialApplication.java
@@ -0,0 +1,13 @@
+package com.reversosocial.reversosocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReversosocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReversosocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reversosocial/reversosocial/ReversosocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reversosocial.reversosocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReversosocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
