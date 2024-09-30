6e39841f565698c01588d3d48ffa192cb40344c2
Deiximar Virriel <deiximar@gmail.com>
2024-09-30 09:14:31 +0200

    Create sector model and add sector to data base

---
+++ src/main/java/com/reversosocial/bean/entity/Sector.java
@@ -0,0 +1,27 @@
+package com.reversosocial.bean.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "sectors")
+public class Sector {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Column(nullable = false, unique = true)
+  private String sector;
+}
--- src/main/java/com/reversosocial/config/data/DataLoader.java
+++ src/main/java/com/reversosocial/config/data/DataLoader.java
@@ -1,13 +1,18 @@
 package com.reversosocial.config.data;
 
+import java.util.Optional;
+
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.reversosocial.bean.entity.ERole;
 import com.reversosocial.bean.entity.Role;
+import com.reversosocial.bean.entity.Sector;
 import com.reversosocial.layer.repository.RoleRepository;
+import com.reversosocial.layer.repository.SectorRepository;
 
-@Component
+@Configuration
 public class DataLoader implements CommandLineRunner {
 
   private RoleRepository roleRepository;
@@ -34,6 +39,31 @@ public class DataLoader implements CommandLineRunner {
       Role femseniorRole = new Role();
       femseniorRole.setRole(ERole.ROLE_FEMSENIOR);
       roleRepository.save(femseniorRole);
+    }
+  }
+
+  @Bean
+  CommandLineRunner initDatabase(SectorRepository repository) {
+    return args -> {
+      insertSectorIfNotExists(repository, "Tecnología");
+      insertSectorIfNotExists(repository, "Administración y Finanzas");
+      insertSectorIfNotExists(repository, "Dirección y Ejecución");
+      insertSectorIfNotExists(repository, "Psicología");
+      insertSectorIfNotExists(repository, "Terapias Alternativas y Desarrollo Personal");
+      insertSectorIfNotExists(repository, "Atención y Cuidados");
+      insertSectorIfNotExists(repository, "Atención al Cliente y Servicios");
+      insertSectorIfNotExists(repository, "Hostelería y Turismo");
+      insertSectorIfNotExists(repository, "Artes y Creatividades");
+      insertSectorIfNotExists(repository, "Servicios a la Comunidad");
+    };
+  }
+
+  private void insertSectorIfNotExists(SectorRepository repository, String sectorName) {
+    Optional<Sector> sector = repository.findBySector(sectorName);
+    if (!sector.isPresent()) {
+      Sector newSector = new Sector();
+      newSector.setSector(sectorName);
+      repository.save(newSector);
     }
   }
 }
