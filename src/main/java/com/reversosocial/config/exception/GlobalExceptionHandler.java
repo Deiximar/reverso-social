3714089143ba6e0a9a1c0af8ac321d13cf7d8850
Deiximar Virriel <deiximar@gmail.com>
2024-09-30 09:35:28 +0200

    Handle Resource not found exception

--- src/main/java/com/reversosocial/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/reversosocial/config/exception/GlobalExceptionHandler.java
@@ -79,4 +79,13 @@ public class GlobalExceptionHandler {
     return new ResponseEntity<>(errorObject, HttpStatus.UNAUTHORIZED);
   }
 
+  @ExceptionHandler(ResourceNotFountException.class)
+  public ResponseEntity<ErrorObject> handleResourceNotFountException(ResourceNotFountException ex) {
+    ErrorObject errorObject = new ErrorObject();
+    errorObject.setStatusCode(HttpStatus.NOT_FOUND.value());
+    errorObject.setMessage(ex.getMessage());
+    errorObject.setTimestamp(new Date());
+    return new ResponseEntity<>(errorObject, HttpStatus.NOT_FOUND);
+  }
+
 }
---
+++ src/main/java/com/reversosocial/config/exception/ResourceNotFountException.java
@@ -0,0 +1,7 @@
+package com.reversosocial.config.exception;
+
+public class ResourceNotFountException extends RuntimeException {
+  public ResourceNotFountException(String message) {
+    super(message);
+  }
+}
