b4728992018114a68e974a7282c528706d87cd1b
Deiximar Virriel <deiximar@gmail.com>
2024-10-01 09:35:18 +0200

    Implement Event Service

--- src/main/java/com/reversosocial/layer/controller/EventController.java
+++ src/main/java/com/reversosocial/layer/controller/EventController.java
@@ -2,6 +2,7 @@ package com.reversosocial.layer.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +18,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping("/api/events")
 @RequiredArgsConstructor
+@PreAuthorize("denyAll()")
 public class EventController {
 
   private final EventService eventService;
 
   @PostMapping
+  @PreAuthorize("hasAuthority('CREATE')")
   public ResponseEntity<EventDto> createEvent(@RequestBody @Valid EventDto eventDto) {
     EventDto createdRoutine = eventService.createEvent(eventDto);
     return new ResponseEntity<EventDto>(createdRoutine, HttpStatus.CREATED);
