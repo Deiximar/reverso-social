15a4fbb53aae1a1a684f5ef1b09d6c03f56bf907
MabelZar <mabel.zar.esp@gmail.com>
2024-10-08 11:59:10 +0200

    resource crud created

---
+++ src/main/java/com/reversosocial/controller/ResourceController.java
@@ -0,0 +1,62 @@
+package com.reversosocial.controller;
+
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.reversosocial.models.dto.ResourceDto;
+import com.reversosocial.service.ResourceService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+@RequestMapping("/api/resource")
+@RequiredArgsConstructor
+@PreAuthorize("denyAll()")
+
+public class ResourceController {
+
+    private final ResourceService resourceService;
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('CREATE')")
+    public ResponseEntity<ResourceDto> createResource(@RequestBody @Valid ResourceDto resourceDto) {
+
+        ResourceDto createdRoutine = resourceService.createResource(resourceDto);
+        return new ResponseEntity<>(createdRoutine, HttpStatus.CREATED);
+
+    }
+
+    @GetMapping
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<List<ResourceDto>> getAllResources() {
+        return new ResponseEntity<>(resourceService.getAllResources(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('UPDATE')")
+    public ResponseEntity<ResourceDto> updateResource(@PathVariable Integer id, @RequestBody ResourceDto resourceDto) {
+        return new ResponseEntity<>(resourceService.updateResource(id, resourceDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('DELETE')")
+    public ResponseEntity<String> deleteResource(@PathVariable Integer id) {
+        return new ResponseEntity<>(resourceService.deleteResource(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<ResourceDto> getResourceById(@PathVariable Integer id) {
+        return new ResponseEntity<>(resourceService.getResourceById(id), HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/reversosocial/service/impl/ResourceServiceImpl.java
@@ -0,0 +1,113 @@
+package com.reversosocial.service.impl;
+
+import java.util.List;
+import org.modelmapper.ModelMapper;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import com.reversosocial.config.exception.ResourceNotFoundException;
+import com.reversosocial.config.exception.UsernameNotFoundException;
+import com.reversosocial.models.dto.ResourceDto;
+import com.reversosocial.models.entity.Resource;
+import com.reversosocial.models.entity.User;
+import com.reversosocial.repository.ResourceRepository;
+import com.reversosocial.repository.UserRepository;
+import com.reversosocial.service.ResourceService;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ResourceServiceImpl implements ResourceService {
+
+    private final ResourceRepository resourceRepository;
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public ResourceDto createResource(ResourceDto resourceDto) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail = authentication.getName();
+
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado."));
+      
+        Resource resource = mapResourceToEntity(resourceDto);
+        resource.setUser(user);
+
+
+        Resource createdResource = resourceRepository.save(resource);
+        return mapResourceToDto(createdResource);
+    }
+
+    @Override
+    public List<ResourceDto> getAllResources() {
+        List<Resource> resources = resourceRepository.findAll();
+        if (resources.isEmpty()) {
+            throw new ResourceNotFoundException("No hay recursos disponibles");
+        }
+        return resources.stream().map(this::mapResourceToDto).toList();
+
+    }
+
+    @Override
+    public String deleteResource(Integer resourceId) {
+        Resource resource = resourceRepository.findById(resourceId)
+                .orElseThrow(() -> new ResourceNotFoundException("Recurso no encontrado"));
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail = authentication.getName();
+
+        if (!isOwnerOrAdmin(resource, userEmail, authentication)) {
+            throw new AccessDeniedException("No tienes permiso para eliminar este recurso.");
+        }
+        resourceRepository.delete(resource);
+        return "Recurso eliminado con éxito.";
+    }
+
+    @Override
+    public ResourceDto updateResource(Integer resourceId, ResourceDto resourceDto) {
+        Resource resource = resourceRepository.findById(resourceId)
+                .orElseThrow(() -> new ResourceNotFoundException("Recurso no encontrado"));
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail = authentication.getName();
+        if (!isOwnerOrAdmin(resource, userEmail, authentication)) {
+            throw new AccessDeniedException("No tienes permiso para modificar este recurso");
+        }
+        resource.setTitle(resourceDto.getTitle());
+        resource.setUrl(resourceDto.getUrl());
+        resource.setDescription(resourceDto.getDescription());
+        resource.setFile(resourceDto.getFile());
+        
+        Resource updatedResource = resourceRepository.save(resource);
+        return mapResourceToDto(updatedResource);
+
+    }
+
+    @Override
+    public ResourceDto getResourceById(Integer resourceId) {
+        Resource resource = resourceRepository.findById(resourceId)
+                .orElseThrow(() -> new ResourceNotFoundException("Recurso no encontrado"));
+        return mapResourceToDto(resource);
+
+    }
+
+    private Resource mapResourceToEntity(ResourceDto resourceDto) {
+        return modelMapper.map(resourceDto, Resource.class);
+    }
+
+    private ResourceDto mapResourceToDto(Resource resource) {
+        ResourceDto resourceDto = modelMapper.map(resource, ResourceDto.class);
+        
+        return resourceDto;
+    }
+
+    private boolean isOwnerOrAdmin(Resource resource, String userEmail, Authentication authentication) {
+        boolean isOwner = resource.getUser().getEmail().equals(userEmail);
+        boolean isAdmin = authentication.getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"));
+        return isOwner || isAdmin;
+    }
+}
