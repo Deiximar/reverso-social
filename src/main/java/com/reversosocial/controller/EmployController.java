0fb276a29832b2998692610314c7c844fa20465d
Deiximar Virriel <deiximar@gmail.com>
2024-10-09 11:58:39 +0200

    Add authorities to controller

--- src/main/java/com/reversosocial/controller/EmployController.java
+++ src/main/java/com/reversosocial/controller/EmployController.java
@@ -2,8 +2,9 @@ package com.reversosocial.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,60 +12,49 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.reversosocial.models.dto.EmployDto;
-import com.reversosocial.models.entity.Employ;
-import com.reversosocial.service.EmployService;
+import com.reversosocial.service.impl.EmployServiceImpl;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/employs")
+@PreAuthorize("denyAll()")
 public class EmployController {
-    
-    @Autowired
-    private EmployService employService;
+
+    private final EmployServiceImpl employService;
 
     @GetMapping
-    public List<EmployDto> getAllEmploys() {
-    return employService.getAllEmploys();
-}
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<List<EmployDto>> getAllEmploys() {
+        return new ResponseEntity<>(employService.getAllEmploys(), HttpStatus.OK);
+    }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EmployDto> getEmployById(@PathVariable Long id) {
-    EmployDto employ = employService.getEmployById(id);
-    if (employ != null) {
-        return ResponseEntity.ok(employ);
-    } else {
-        return ResponseEntity.notFound().build();
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<EmployDto> getEmployById(@PathVariable Integer id) {
+        return new ResponseEntity<EmployDto>(employService.getEmployById(id), HttpStatus.OK);
     }
-}
 
+    @PostMapping
+    @PreAuthorize("hasAuthority('CREATE')")
+    public ResponseEntity<EmployDto> createEmployOffer(@Valid @RequestBody EmployDto employDto) {
+        EmployDto createdEmploy = employService.createEmploy(employDto);
+        return new ResponseEntity<EmployDto>(createdEmploy, HttpStatus.CREATED);
+    }
 
-    @PostMapping("/create")
-    public ResponseEntity<EmployDto> createEmployOffer(
-    @RequestParam("position") String position,
-    @RequestParam("description") String description,
-    @RequestParam("cvFile") MultipartFile cvFile,
-    @RequestParam("sectorId") int sectorId) {
-
-    EmployDto createdEmploy = employService.createEmployOffer(position, description, cvFile, sectorId);
-
-    return ResponseEntity.ok(createdEmploy);
-}
     @PutMapping("/{id}")
-    public ResponseEntity<Employ> updateEmploy(@PathVariable Long id, @RequestBody Employ employDetails) {
-        Employ employ = employService.updateEmployOffer(id, employDetails);
-        return ResponseEntity.ok(employ);
+    @PreAuthorize("hasAuthority('UPDATE')")
+    public ResponseEntity<EmployDto> updateEmploy(@PathVariable Integer id, @RequestBody EmployDto employDto) {
+        return new ResponseEntity<EmployDto>(employService.updateEmploy(id, employDto), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteEmployOffer(@PathVariable Long id) {
-        employService.deleteEmployOffer(id);
-        return ResponseEntity.noContent().build();
-    } 
+    @PreAuthorize("hasAuthority('DELETE')")
+    public ResponseEntity<String> deleteEmployOffer(@PathVariable Integer id) {
+        return new ResponseEntity<>(employService.deleteEmploy(id), HttpStatus.OK);
+    }
 }
-
-       
-
