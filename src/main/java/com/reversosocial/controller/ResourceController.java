6584acfeff53281d65289f892211507be44cff47
paolaperdomo <paolaperdomo079@gmail.com>
2024-10-11 15:55:14 +0200

    EndPoint Resource fix

--- src/main/java/com/reversosocial/controller/ResourceController.java
+++ src/main/java/com/reversosocial/controller/ResourceController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
-@RequestMapping("/api/resource")
+@RequestMapping("/api/resources")
 @RequiredArgsConstructor
 @PreAuthorize("denyAll()")
 
